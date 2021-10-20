219ade53c67fc0228f108480b32fe0cb61705f37
winstonthewolf <31859922+winstonthewolf@users.noreply.github.com>
2021-10-20 09:39:43 -0700

    Added Scratch2.java

---
+++ src/Methods/Scratch2.java
@@ -0,0 +1,12 @@
+package Methods;
+
+import java.util.*;
+public class Scratch2 {
+
+    public static void main(String []args) {
+
+        Integer[] intArray = {1,2,3};
+        System.out.println(intArray[0]+ intArray[1]+ intArray[2]);
+    }
+
+}
--- src/Methods/scratch.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class scratch {
-
-    public static void main(String []args) {
-        int i = 9;
-        System.out.println(i/2);
-    }
-
-}
