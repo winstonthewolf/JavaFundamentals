496368ffe6cfc0ca52c683a97342041c9aa72525
Winston The Wolf <31859922+winstonthewolf@users.noreply.github.com>
2021-10-25 06:14:11 -0700

    Almoost finished but messed up in the while loops

--- src/ObjectsAndClasses/Accounts.java
+++ src/ObjectsAndClasses/Accounts.java
@@ -5,51 +5,67 @@ import java.util.Scanner;
 public class Accounts {
     public static void main(String[] args) {
 
-        Account [] storedAccounts = new Account[10];
-        for(int i = 0; i<10; i++){
-            storedAccounts[i] =new Account(i,100);
+        Account[] storedAccounts = new Account[10];
+        for (int i = 0; i < 10; i++) {
+            storedAccounts[i] = new Account(i, 100);
         }
         Scanner input = new Scanner(System.in);
-        int id = input.nextInt();
-        if ((id < storedAccounts.length )&& (id >= -1) ){
-           System.out.println("Main menu");
-           System.out.println("0: Check balance");
-           System.out.println("1: Withdraw");
-           System.out.println("2: Deposit");
-           System.out.println("3: exit");
-           System.out.println("Enter a choice: ");
-
-           ///TODO Make above if statement to while loop and put the printed statements in a method
-
-           switch(id){
-
-               case 1: System.out.println("Current Balance: "+ storedAccounts[id].getBalance());
-
-               case 2: System.out.println("How much do you want to withdraw?");
-                        System.out.println("Enter amount: ");
-                        double amountWithdrawn = input.nextDouble();
-                        storedAccounts[id].withdraw(amountWithdrawn);
-                        System.out.println("SUCCESS");
-
-               case 3: System.out.println("How much to deposit? ");
-                        System.out.println("Enter amount: ");
-                        double amountDeposited = input.nextDouble();
-                        storedAccounts[id].deposit(amountDeposited);
-                        System.out.println("SUCCESS");
-
-               case 4: System.out.println("Goodbye");
 
 
-           }
+        int id;
+        do {
+            System.out.println("Enter ID");
+            id = input.nextInt();
+        } while((id > storedAccounts.length) || (id < 0));
+        
+        if ((id < storedAccounts.length) && (id >= 0)) {
+            printMenu();
+            int choice;
+            choice = input.nextInt();
+
+            while(choice < 0 || choice > storedAccounts.length){   
+                System.out.println("Enter valid option");
+                choice = input.nextInt();
+            }
+                while(choice <= 3 && choice >= 0) {
+                    switch (choice) {
+                        case 0 -> System.out.println("Current Balance: " + storedAccounts[id].getBalance());
+                        case 1 -> {
+                            System.out.println("How much do you want to withdraw?");
+                            System.out.println("Enter amount: ");
+                            double amountWithdrawn = input.nextDouble();
+                            storedAccounts[id].withdraw(amountWithdrawn);
+                            System.out.println("SUCCESS");
+                        }
+                        case 2 -> {
+                            System.out.println("How much to deposit? ");
+                            System.out.println("Enter amount: ");
+                            double amountDeposited = input.nextDouble();
+                            storedAccounts[id].deposit(amountDeposited);
+                            System.out.println("SUCCESS");
+                        }
+                        case 3 -> System.out.println("Goodbye");
+                    }
+                    System.out.println("Enter valid option!");
+                    printMenu();
+                    choice = input.nextInt();
+                }
+            }
 
         }
 
 
 
 
-//        /if((input.nextInt()< storedAccounts.length) && input.nextInt() >= 0){
 
-//        /i
-        //TODO method getID == id to retrieve account menu
+    private static void  printMenu(){
+
+        System.out.println("*******Main menu********");
+        System.out.println("0: Check balance");
+        System.out.println("1: Withdraw");
+        System.out.println("2: Deposit");
+        System.out.println("3: exit");
+        System.out.println("Enter a choice: ");
+
     }
 }
