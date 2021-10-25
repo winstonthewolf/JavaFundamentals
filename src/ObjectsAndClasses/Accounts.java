3d6d8ebbb68c3f57d62280e17d8ee13041dba347
Winston The Wolf <31859922+winstonthewolf@users.noreply.github.com>
2021-10-25 08:25:42 -0700

    Figured out loops for menu. Class finished.

--- src/ObjectsAndClasses/Accounts.java
+++ src/ObjectsAndClasses/Accounts.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Accounts {
     public static void main(String[] args) {
 
-        Account[] storedAccounts = new Account[10];
+        Account[] storedAccounts = new Account[10];                 //Make 10 accounts each with 100$
         for (int i = 0; i < 10; i++) {
             storedAccounts[i] = new Account(i, 100);
         }
@@ -13,52 +13,49 @@ public class Accounts {
 
 
         int id;
-        do {
+        do {                                                        //Enter valid ID. ID is 0-9 using accounts array.
             System.out.println("Enter ID");
             id = input.nextInt();
         } while((id > storedAccounts.length) || (id < 0));
         
-        if ((id < storedAccounts.length) && (id >= 0)) {
-            printMenu();
-            int choice;
-            choice = input.nextInt();
 
-            while(choice < 0 || choice > storedAccounts.length){   
+        int choice;
+
+        while(true) {                                     //Menu populated and choice verification system. System breaks out of loop and ends when choice 3 is chosen
+
+            printMenu();
+            choice = input.nextInt();
+            while (choice < 0 || choice > 3) {                  //If out of choice out of bounds retry until it is a valid number
                 System.out.println("Enter valid option");
                 choice = input.nextInt();
             }
-                while(choice <= 3 && choice >= 0) {
-                    switch (choice) {
-                        case 0 -> System.out.println("Current Balance: " + storedAccounts[id].getBalance());
-                        case 1 -> {
-                            System.out.println("How much do you want to withdraw?");
-                            System.out.println("Enter amount: ");
-                            double amountWithdrawn = input.nextDouble();
-                            storedAccounts[id].withdraw(amountWithdrawn);
-                            System.out.println("SUCCESS");
-                        }
-                        case 2 -> {
-                            System.out.println("How much to deposit? ");
-                            System.out.println("Enter amount: ");
-                            double amountDeposited = input.nextDouble();
-                            storedAccounts[id].deposit(amountDeposited);
-                            System.out.println("SUCCESS");
-                        }
-                        case 3 -> System.out.println("Goodbye");
-                    }
-                    System.out.println("Enter valid option!");
-                    printMenu();
-                    choice = input.nextInt();
+            switch (choice) {                                   //Choices
+                case 0 -> System.out.println("Current Balance: " + storedAccounts[id].getBalance());
+                case 1 -> {
+                    System.out.println("How much do you want to withdraw?");
+                    System.out.println("Enter amount: ");
+                    double amountWithdrawn = input.nextDouble();
+                    storedAccounts[id].withdraw(amountWithdrawn);
+                    System.out.println("SUCCESS");
+                }
+                case 2 -> {
+                    System.out.println("How much to deposit? ");
+                    System.out.println("Enter amount: ");
+                    double amountDeposited = input.nextDouble();
+                    storedAccounts[id].deposit(amountDeposited);
+                    System.out.println("SUCCESS");
+                }
+                case 3 -> {
+                    System.out.println("Goodbye");
+                    System.exit(0);
                 }
             }
-
         }
 
 
+    }
 
-
-
-    private static void  printMenu(){
+    private static void  printMenu(){                                //Prints out menu
 
         System.out.println("*******Main menu********");
         System.out.println("0: Check balance");
