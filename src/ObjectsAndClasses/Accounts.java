e58ceb1b6b4a0dbdabba9450f5149b4b668e844e
Winston The Wolf <31859922+winstonthewolf@users.noreply.github.com>
2021-10-24 18:35:33 -0700

    Almost finished solution see TODO.

--- src/ObjectsAndClasses/Accounts.java
+++ src/ObjectsAndClasses/Accounts.java
@@ -1,5 +1,7 @@
 package ObjectsAndClasses;
 
+import java.util.Scanner;
+
 public class Accounts {
     public static void main(String[] args) {
 
@@ -7,7 +9,47 @@ public class Accounts {
         for(int i = 0; i<10; i++){
             storedAccounts[i] =new Account(i,100);
         }
+        Scanner input = new Scanner(System.in);
+        int id = input.nextInt();
+        if ((id < storedAccounts.length )&& (id >= -1) ){
+           System.out.println("Main menu");
+           System.out.println("0: Check balance");
+           System.out.println("1: Withdraw");
+           System.out.println("2: Deposit");
+           System.out.println("3: exit");
+           System.out.println("Enter a choice: ");
 
+           ///TODO Make above if statement to while loop and put the printed statements in a method
+
+           switch(id){
+
+               case 1: System.out.println("Current Balance: "+ storedAccounts[id].getBalance());
+
+               case 2: System.out.println("How much do you want to withdraw?");
+                        System.out.println("Enter amount: ");
+                        double amountWithdrawn = input.nextDouble();
+                        storedAccounts[id].withdraw(amountWithdrawn);
+                        System.out.println("SUCCESS");
+
+               case 3: System.out.println("How much to deposit? ");
+                        System.out.println("Enter amount: ");
+                        double amountDeposited = input.nextDouble();
+                        storedAccounts[id].deposit(amountDeposited);
+                        System.out.println("SUCCESS");
+
+               case 4: System.out.println("Goodbye");
+
+
+           }
+
+        }
+
+
+
+
+//        /if((input.nextInt()< storedAccounts.length) && input.nextInt() >= 0){
+
+//        /i
         //TODO method getID == id to retrieve account menu
     }
 }
