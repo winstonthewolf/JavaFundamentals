7149d57fcf5dd0dae74e749bc9064de9809c47f0
winstonthewolf <31859922+winstonthewolf@users.noreply.github.com>
2021-11-04 16:51:39 -0700

    Done with testing functionality of queue

--- src/ObjectOrientedThinking/Queue.java
+++ src/ObjectOrientedThinking/Queue.java
@@ -3,9 +3,23 @@ package ObjectOrientedThinking;
 public class Queue {
 
 
-    //TODO: Test class for functionality
+    /* TODO: Test class for functionality: DONE
+    * Finish last part of problem
+    *
+    *
+    * */
 
     public static void main(String [] args){
+
+        Queue myQueue = new Queue();
+        myQueue.enqueue(5);
+        myQueue.enqueue(6);
+        myQueue.enqueue(7);
+        System.out.println(myQueue.empty());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.empty());
 
     }
     int [] elements;
