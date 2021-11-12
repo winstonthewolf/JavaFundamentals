40c72de3f13016437c8cb6490fc6d7ce78b9a21d
Winston The Wolf <31859922+winstonthewolf@users.noreply.github.com>
2021-11-12 14:57:32 -0800

    Finished compare() method. Still need to clean it up.

--- src/ObjectOrientedThinking/MyString2.java
+++ src/ObjectOrientedThinking/MyString2.java
@@ -13,25 +13,42 @@ public class MyString2 {
     public int compare(String s){
 
         boolean match = true;
-        for (int i =0 ; i < str.length(); i++){                                                    //TODO: Can maybe simplify if else statement to one and merge both conditionals when doing the < and > operations
-
-            if(str.charAt(i) == s.charAt(i)){
+        String longestString; //Ensures the longest string will be used as the number of character iterations
+        String shortString;
+        if(str.length() > s.length()) {
+            longestString = str;
+            shortString = s;
+        } else {
+            longestString = s;
+            shortString = str;
+        }
+        for (int i = 0 ; i < shortString.length(); i++){                                                    //TODO: Can maybe simplify if else statement to one and merge both conditionals when doing the < and > operations
+                  
+            if(shortString.charAt(i) == longestString.charAt(i)){
                continue;
             }else{
-                if((int)str.charAt(i) < (int)s.charAt(i)){ // if object character precedes argument character
+                if((int)longestString.charAt(i) < (int)shortString.charAt(i)){ // if object character precedes argument character
                     match = false;
-                    return str.charAt(i) - s.charAt(i);          //Will be a negative result. Will tell how many characters of argument string (exceeded)behind the object character is.
+                    return longestString.charAt(i) - shortString.charAt(i);          //Will be a negative result. Will tell how many characters of argument string (exceeded)behind the object character is.
                 }else{
-                       if((int)str.charAt(i) > (int)s.charAt(i)) {
+                       if((int)longestString.charAt(i) > (int)shortString.charAt(i)) {
                            match = false;
-                           return s.charAt(i) - str.charAt(i); //Will do the same as above but now opposites
+                           return shortString.charAt(i) - longestString.charAt(i); //Will do the same as above but now opposites
                        }
                 }
             }
         }
-
-        return 0; //Strings are equal lexicographically
+        if(longestString.length() > shortString.length()){
+            return longestString.length() - shortString.length();// if all characters are equal then return 0 or a number n representing how many extra characters there were (always a positive number).. Short string precedes longest String lexicographically
+        }else
+            return 0; //Strings are equal lexicographically
     }
+
+    /*
+    There are 3 possible return codes. 0, Strings are lexographically equal. N < 0, when two characters are not identical, this number represents the number of characters away from eachoothers string respective index
+    N > 0, a number n representing how many extra characters there were in the longest string  (always a positive number)
+     */
+
 /*
    TODO: If there is no index position at which they differ, then the shorter string lexicographically precedes the longer string. In this case, compareTo returns the difference of the lengths of the strings -- that is, the value:
 
@@ -41,9 +58,13 @@ public class MyString2 {
     TODO:      Returns: the value 0 if the argument string is equal to this string; a value less than 0 if this string is lexicographically less than the string argument; and a value greater than 0 if this string is lexicographically greater than the string argument.
  */
 
+    /*
+
+    Need to figure out way to discern between when the returned value is from the length of strings vs when its a character difference
+     */
 
     public static void main(String []args){
-        MyString2 str = new MyString2("Hello");
+        MyString2 str = new MyString2("Helli");
         System.out.println(str.compare("Hello"));
     }
 }
