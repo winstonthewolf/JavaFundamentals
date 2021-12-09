9a4ed96361172098b2aa2ddf0936d7c31d577320
Winston The Wolf <31859922+winstonthewolf@users.noreply.github.com>
2021-12-09 06:05:41 -0800

    Completed maxArrayListElement class

---
+++ src/InheritanceAndPolymorphism/maxArrayListElement.java
@@ -0,0 +1,26 @@
+package InheritanceAndPolymorphism;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class maxArrayListElement {
+
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+        int nextNum;
+        nextNum = input.nextInt();
+        while(nextNum  != 0){
+            nextNum = input.nextInt();
+            list.add(nextNum);
+        }
+        System.out.println(max(list));
+    }
+
+    public static Integer max(ArrayList<Integer> list){
+
+        return java.util.Collections.max(list);
+    }
+}
